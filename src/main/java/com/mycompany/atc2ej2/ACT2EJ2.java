a887b01e71086b42c7e53bab87484abb51eac546
prueba <prueba@DESKTOP-US4O5C1>
2024-09-29 22:52:00 -0300

    Actividad 5

---
+++ src/main/java/com/mycompany/atc2ej2/ACT2EJ2.java
@@ -0,0 +1,26 @@
+
+package com.mycompany.atc2ej2;
+
+
+public class ACT2EJ2 {
+
+    public static void main(String[] args) {
+        
+        Perro objPerro = new Perro(":negro", ":Mediano");
+        
+        objPerro.setRaza(":Galgo");
+        objPerro.mostrarDatos();
+     
+        Gato objGato = new Gato(":Blanco", ":Chico");
+        
+        objGato.setPelaje(":Largo");
+        objGato.mostrarDatos();
+        
+        Caballo objCaballo = new Caballo(":Marron", ":Grande");
+        
+        objCaballo.setDeporte("Polo");
+        objCaballo.mostrarDatos();
+         
+      
+    }
+}
